5c6128ea545d63d8fb648b0b090cc0a9efb1cf3c
Brandon Beeks <brandon@beeks.me>
2015-12-13 20:55:49 -0700

    Clean up instructions

--- src/App.js
+++ src/App.js
@@ -20,18 +20,18 @@ module.exports = "Hello World!";
 
 // 5. Define the entry file as this one:
 
-// entry: './App.js'
+// entry: './src/App.js'
 
 // 6. Tell Webpack where to save the compiled file by defining `output`:
 
 // output: {
-//   path: __dirname + '/pubic/js',
+//   path: './public/js',
 //   filename: 'bundle.js'
 // }
 
 // 7. Include the bundled file in the body of index.html
 // 8. Run `webpack` - this will create `bundle.js` in the public/js directory
-// 9. Open `index.html` in a browser and you should see "Hello World!" as defined at the top of this file
+// 9. Open `index.html` in a browser and you should see "Hello World!" in the console, as defined at the top of this file
 
 
 
@@ -40,18 +40,21 @@ module.exports = "Hello World!";
 /// BABEL
 //////////////////////////////////////\
 // 1. Install the Babel loader for Webpack `npm install babel-loader --save-dev`
-// 2. Define it as a loader for Webpack:
+// 2. Install the Babel react preset `npm install babel-preset-react --save-dev`
+// 3. Define it as a loader for Webpack:
 
-// loaders: [
-//   {
-//     test: /\.js$/, // all files that end in '.js' will be ran through this loader
-//     include: /src/, // only run through files located in the `src` directory
-//     exclude: /node_modules/, // ignore the `node_modules` directory
-//     loader: 'babel-loader'
-//   }
-// ]
+// module: {
+//   loaders: [
+//     {
+//       test: /\.js$/, // all files that end in '.js' will be ran through this loader
+//       include: /src/, // only run through files located in the `src` directory
+//       exclude: /node_modules/, // ignore the `node_modules` directory
+//       loader: 'babel?presets[]=react'
+//     }
+//   ]
+// }
 
-// 3. Run `webpack` again and make sure you can still load `index.html` in a browser
+// 4. Run `webpack` again and make sure you can still load `index.html` in a browser
 
 
 
@@ -103,17 +106,9 @@ module.exports = "Hello World!";
 //////////////////////////////////////
 // 1. Within 'componentDidMount', define a `setTimeout` method that will update the `textToDisplay` state value to your name after 2 seconds
 // 2. Run `webpack` and make sure you can still load `index.html` in a browser
-// 3. After 2 seconds, you should see the displayed text change from "Hello World!" to "Hello [Your Name]!"
-
-
-
-
-//////////////////////////////////////
-/// REACT BASICS: setState
-//////////////////////////////////////
-// 1. Use 'getInitialState' to define a new state variable within the component called `textToDisplay`, and set it's value to `Hello World!`
-// 2. Replace the "World" text in the `render` method with the value of this new state variable: `this.state.textToDisplay`
-// 3. Run `webpack` and make sure you can still load `index.html` in a browser
+// 3. After 2 seconds, you should see the displayed text change from "Hello World!" to "Hello [Your Name]!" (hint: it won't work!)
+// 4. Update `setTimeout` to use a fat-arrow method to avoid scope issues
+// 5. Run 'webpack' and take another look...it should work now
 
 
 
@@ -124,7 +119,7 @@ module.exports = "Hello World!";
 // 1. Give your React component a css class name by defining the `className` attribute
 // 2. Use the css class name to change the displayed text to red
 // 3. Run `webpack` and make sure you can still load `index.html` in a browser
-// 3. Remove the class name and use the `style` attribute to change the displayed text to red
+// 3. Remove the class name and use the `style` attribute to change the displayed text to blue
 // 3. Run `webpack` and make sure you can still load `index.html` in a browser
 
 
